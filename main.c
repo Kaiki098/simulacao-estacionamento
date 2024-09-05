be5383a5a92e361e78a9edfcf80adc7d1dc50ef1
kaiki <kaikialvarengasouza098@gmail.com>
2024-09-05 16:34:01 -0300

    feature: menu

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ typedef struct {
 #include "ruaFila.h"
 #include <string.h>
 // Depois de fazer todas as funcoes, separar as funcoes em outro arquivo
-#define MAX_CARROS 5 // Numero maximo de carros no estacionamento
+#define MAX_CARROS 5  // Numero maximo de carros no estacionamento
+#define NUMERO_RUAS 2 // Numero de ruas sendo monitoradas
 
 // Funcoes para gerenciamento do estacionamento
 // funcoes para retirado de carros
@@ -28,7 +29,6 @@ void retirarCarro(TipoFila *estacionamento, TipoCarro carro) { // Void ou Int
      *   se nao tiver um com maior prioridade o mais a frente deve estacionar;
      */
 }
-
 
 // Funcoes para estacionar carro
 int estacionamentoEstaCheio(TipoFila *estacionamento) {
@@ -61,6 +61,18 @@ void estacionarCarro(TipoCarro carro, TipoFila *estacionamento, TipoFila *ruaMon
     }
 }
 
+void exibirRuas(TipoFila ruas[]) {
+    int i;
+    for (i = 0; i < NUMERO_RUAS; i++) {
+        if (ruas[i].frente->prox == NULL) {
+            printf("A rua %d está vazia\n", i+1);
+            continue;
+        }
+        
+        exibe(ruas[i]);
+    }
+}
+
 int main() {
 
     TipoFila BomJesusDosPassos; // Estacionamento
@@ -69,29 +81,70 @@ int main() {
     TipoFila MonsenhorJoaoPedro; // Entrada/Fila de espera
     FFVazia(&MonsenhorJoaoPedro);
 
+    TipoFila ruas[NUMERO_RUAS];
+    ruas[0] = BomJesusDosPassos;
+    ruas[1] = MonsenhorJoaoPedro;
+
     TipoCarro carro;
-    carro.deslocamento = 0;
-    strcpy(carro.placa, "ADSCFEV");
-    carro.prioridade = 1;
 
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-    estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
-
-    printf("Estacionamento: \n");
-    exibe(BomJesusDosPassos);
-    printf("Espera: \n");
-    exibe(MonsenhorJoaoPedro);
     // TipoFila JaimeGoncalves; Eh necessaria?
     // FFVazia(&JaimeGoncalves);
+    int opcao = 0;
 
-    /*
-     Menu:
-        RetirarCarro()
-        EstacionarCarro() deve imprimir/retornar que o carro foi estacionado
-    */
+    do {
+        printf("1. Estacionar carro\n");
+        printf("2. Retirar carro\n");
+        printf("3. Exibir ruas\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar(); // Limpa o buffer deixado pelo scanf
+
+        switch (opcao) {
+        case 1:
+            printf("Digite a placa do carro: ");
+            fgets(carro.placa, 10, stdin);
+            // Remove o newline do final da string, se presente
+            carro.placa[strcspn(carro.placa, "\n")] = '\0';
+
+            printf("O carro tem alguma prioridade, [S]/[N]: ");
+            char resposta[2];
+            fgets(resposta, 2, stdin);
+            if (strcmp("S", resposta) == 0) {
+                carro.prioridade = 1;
+            } else {
+                carro.prioridade = 0;
+            }
+
+            carro.deslocamento = 0;
+
+            estacionarCarro(carro, &BomJesusDosPassos, &MonsenhorJoaoPedro);
+
+            printf("%s %d\n", carro.placa, carro.prioridade);
+
+            getchar();
+            break;
+        case 2:
+            printf("Digite a placa do carro: ");
+            char placa[10];
+            fgets(placa, 10, stdin);
+
+            break;
+        case 3:
+            exibirRuas(ruas);
+            break;
+        default:
+            if (opcao != 0) {
+                printf("Voce digitou uma opcao invalida. Por favor, digite outro valor.\n");
+            }
+            break;
+        }
+
+        printf("\nPressione Enter para continuar...");
+        getchar();
+        system("cls");
+        system("clear");
+    } while (opcao != 0);
+
     return 0;
 }
