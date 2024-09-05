824fb42747489b1ad135368b348d8d0192c32be4
kaiki <kaikialvarengasouza098@gmail.com>
2024-09-05 08:37:08 -0300

    tarefa: adiciona estacionar.c e gitignore
    estacionar.c terá as funções de estacioanr e futuramente será uma lib (.h)

--- main.c
+++ main.c
@@ -9,33 +9,24 @@ typedef struct {
 */
 
 #include "ruaFila.h"
-#define MAX = 5 // Numero maximo de carros no estacionamento
+// Depois de fazer todas as funcoes, separar as funcoes em outro arquivo
+#define MAX_CARROS 5 // Numero maximo de carros no estacionamento
 
 // Funcoes para gerenciamento do estacionamento
 // TODO
-void retirarCarro() { // Void ou Int
+void retirarCarro(TipoFila *estacionamento, TipoCarro carro) { // Void ou Int
 
     /**
      * - Deve utilizar a fila ruaAuxiliar para quando o carro nao eh o primeiro da fila
      *   e garantir que as filas estao na mesma ordem com a retirada do carro;
-     * 
+     *
      * - Deve retornar/exibir o número de vezes que o carro foi deslocado
      *   dentro da fila BomJesusDosPassos, incluindo a partida, mas nao a chegada;
-     * 
+     *
      * - Deve estacionar os carros que estao na fila de espera com maior prioriade,
      *   se nao tiver um com maior prioridade o mais a frente deve estacionar;
-    */
-}
-
-// TODO
-void estacionarCarro() {
-    /** 
-     *  Deve verificar se a fila BomJesusDosPassos esta cheia
-     *      Se estiver cheia deve ser enfileirado na fila MonsenhorJoaoPedro
-     *      Caso nao estiver cheia deve imprimir/retornar que o carro foi estacionado
      */
 }
-
 
 int main() {
 
@@ -47,9 +38,6 @@ int main() {
 
     // TipoFila JaimeGoncalves; Eh necessaria?
     // FFVazia(&JaimeGoncalves);
-
-    TipoFila ruaAuxiliar; // Usada para auxiliar a retirada de carros
-    FFVazia(&ruaAuxiliar);// Talvez seja usada apenas dentro de funções como uma variavel auxiliar
 
     /*
      Menu:
