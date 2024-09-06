db84808b19e02dcf52a790a686bcd3537aa567a6
Richard <147954178+Ric-dll@users.noreply.github.com>
2024-09-05 23:31:33 -0300

    Função retirarCarro().  main.c
    Foi mantido os comentários, porém está com um problema de que caso o o carro escolhido for o último, ele não encontra, assim procedendo o com maior prioridade para a fila.
    
    Também realizei o teste na função principal, adicionando alguns espaçamentos entre o crud.
    
    OBS: Não sei se era assim, Todos os carros deveriam voltar ao estacionamento ou somente o com maior prioridade e após se inicia o processo de ADD manual novamente?

--- main.c
+++ main.c
@@ -16,8 +16,10 @@ typedef struct {
 
 // Funcoes para gerenciamento do estacionamento
 // funcoes para retirado de carros
-void retirarCarro(TipoFila *estacionamento, TipoCarro carro) { // Void ou Int
-
+void retirarCarro(TipoFila *ruaMonsenhor, TipoFila *estacionamento, TipoCarro carro){ // Void ou Int
+	
+	//Foi adicionado o parametro 'ruaMonsenhor' que atuará como uma "Fila de Espera"
+	
     /**
      * - Deve utilizar a fila ruaAuxiliar para quando o carro nao eh o primeiro da fila
      *   e garantir que as filas estao na mesma ordem com a retirada do carro;
@@ -28,6 +30,54 @@ void retirarCarro(TipoFila *estacionamento, TipoCarro carro) { // Void ou Int
      * - Deve estacionar os carros que estao na fila de espera com maior prioriade,
      *   se nao tiver um com maior prioridade o mais a frente deve estacionar;
      */
+     
+    //A variavel filaAuxiliar ira atuar como uma fila temporaria para armazenar os carros posteriores ao que sera removido
+    TipoFila filaAuxiliar; FFVazia(&filaAuxiliar);
+  
+	//Variaveis de apoio referente ao carro e o flag de encontre 
+    TipoCarro carroAtual;
+    int flag = 0;
+
+    //Percorrer a fila do estacionamento até esvaziar em busca do carro
+    while(!Vazia(*estacionamento)){
+        //Começa desenfileirando, devido a simulacao, e armazenando o mesmo no campo temporario
+        Desenfileira(estacionamento, &carroAtual);
+
+		//Verificacao de placa
+        if(strcmp(carroAtual.placa, carro.placa) == 0){
+            //Se encontrou o carro a ser retirado...
+            flag = 1; //Sinaliza sinal 1
+            printf("\nCarro de placa: %s removido. Deslocamentos: %d\n", carroAtual.placa, carroAtual.deslocamento + 1);
+            break;
+        }else{
+            //Se não, irá movendo os demais para a rua auxiliar ate encontra-lo
+			carroAtual.deslocamento++; //Incrementar deslocamento simbolizando a rua
+			Enfileira(carroAtual, &filaAuxiliar); //Enfileirando na filaAuxiliar
+        }
+    }
+
+	//Verificação de flag 0, Se Nao achou...
+    if(!flag){
+        printf("\nCarro de placa: %s não encontrado no estacionamento...\n", carro.placa);
+    }
+	
+    //Retornando os carros da 'ruaAuxiliar' para o estacionamento
+    while(!Vazia(filaAuxiliar)){
+        Desenfileira(&filaAuxiliar, &carroAtual);
+        Enfileira(carroAtual, estacionamento);
+    }
+
+    //Se houver fila de espera, verificar se ha algum carro com prioridade
+    if(!Vazia(*ruaMonsenhor)){
+        Desenfileira(ruaMonsenhor, &carroAtual);
+
+        if(carroAtual.prioridade == 1){
+            printf("\nCarro de placa: %s com PRIORIDADE entrou no estacionamento.\n", carroAtual.placa);
+        }else{
+            printf("\nCarro de placa: %s entrou no estacionamento.\n", carroAtual.placa);
+        }
+        Enfileira(carroAtual, estacionamento);
+    }
 }
 
 // Funcoes para estacionar carro
@@ -65,15 +115,14 @@ void exibirRuas(TipoFila ruas[]) {
     int i;
     for (i = 0; i < NUMERO_RUAS; i++) {
         if (ruas[i].frente->prox == NULL) {
-            printf("A rua %d está vazia\n", i+1);
+            printf("\nA rua %d está vazia\n", i+1);
             continue;
         }
-        
         exibe(ruas[i]);
     }
 }
 
-int main() {
+int main(){
 
     TipoFila BomJesusDosPassos; // Estacionamento
     FFVazia(&BomJesusDosPassos);
@@ -92,25 +141,26 @@ int main() {
     int opcao = 0;
 
     do {
-        printf("1. Estacionar carro\n");
-        printf("2. Retirar carro\n");
-        printf("3. Exibir ruas\n");
-        printf("0. Sair\n");
+		printf("\nSISTEMA DE ESTACIONAMENTO\n");
+        printf("\n 1. Estacionar carro\n");
+        printf(" 2. Retirar carro\n");
+        printf(" 3. Exibir ruas\n");
+        printf(" 0. Sair\n\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
         getchar(); // Limpa o buffer deixado pelo scanf
 
         switch (opcao) {
         case 1:
-            printf("Digite a placa do carro: ");
+            printf("\nDigite a placa do carro: ");
             fgets(carro.placa, 10, stdin);
             // Remove o newline do final da string, se presente
             carro.placa[strcspn(carro.placa, "\n")] = '\0';
 
-            printf("O carro tem alguma prioridade, [S]/[N]: ");
+            printf("\nO carro tem alguma prioridade, [S]/[N]: ");
             char resposta[2];
             fgets(resposta, 2, stdin);
-            if (strcmp("S", resposta) == 0) {
+            if (strcmp("S", resposta) == 0 || strcmp("s", resposta) == 0) {
                 carro.prioridade = 1;
             } else {
                 carro.prioridade = 0;
@@ -125,17 +175,27 @@ int main() {
             getchar();
             break;
         case 2:
-            printf("Digite a placa do carro: ");
+            printf("\nDigite a placa do carro: ");
             char placa[10];
             fgets(placa, 10, stdin);
-
+            // Remove o newline do final da string, se presente
+			placa[strcspn(placa, "\n")] = '\0';
+			
+			//TipoCarro temporário para remocao
+			TipoCarro carroBusca;
+			//Atribuindo a placa obtida anteriormente ao carroBusca
+			strcpy(carroBusca.placa, placa);
+			
+			//Chamando a funcao
+			retirarCarro(&BomJesusDosPassos, &MonsenhorJoaoPedro, carroBusca);
+			
             break;
         case 3:
             exibirRuas(ruas);
             break;
         default:
             if (opcao != 0) {
-                printf("Voce digitou uma opcao invalida. Por favor, digite outro valor.\n");
+                printf("\nVoce digitou uma opcao invalida. Por favor, digite outro valor.\n");
             }
             break;
         }
